3c5847bacca2f3346abeb131cd0af265317b8ad8
rosagross <43537429+rosagross@users.noreply.github.com>
2019-05-16 10:45:31 +0200

    Deleted duplicate Chromosome
    Versehentlich doppelt gehabt.. ;)

--- Cromosome.java
+++
@@ -1,9 +0,0 @@
-/**
- * An Assignment should be an array object that represent the Chromosomes.
- * The single entries in the array are single genes.
- * @author Rosa
- */
-public class Chromosome {
-	
-	
-}
